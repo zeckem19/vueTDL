b590bbd5fb3c0b2b41c2f9dbe1abf9b3fb917254
zeckem19 <zeckem19@gmail.com>
2019-11-02 15:03:07 +0800

    added checkbox

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -1,6 +1,19 @@
 new Vue({
     el: "#app",
     data:{
-        header: "Hey world"
+        header: "Hey world",
+        newEntry:"",
+        todoList: []
+    },
+    methods: {
+        addEntry: function(){
+            if(this.newEntry){
+                this.todoList.push({value: this.newEntry, isDone:false});
+                this.newEntry = "";
+            }
+        },
+        removeEntry: function(index){
+            this.todoList.splice(index, 1);
+        }
     }
 })
