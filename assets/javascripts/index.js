251fd04127ce32178e71142748cb5ef45e625721
zeckem19 <zeckem19@gmail.com>
2019-11-02 15:35:16 +0800

    added unfinished entries

--- assets/javascripts/index.js
+++ assets/javascripts/index.js
@@ -12,9 +12,12 @@ new Vue({
             },
             deep: true
         }
-
     },
-
+    computed: {
+        unfinishedEntries: function(){
+            return this.todoList.filter(entry => !entry.isDone);
+        }
+    },
     methods: {
         addEntry: function(){
             if(this.newEntry){
@@ -25,5 +28,22 @@ new Vue({
         removeEntry: function(index){
             this.todoList.splice(index, 1);
         }
-    }
+    },
+    beforeCreate: function(){
+        console.log('before create');
+    },
+    created: function(){
+        console.log('created');
+    },
+
+    beforeMount: function(){
+        console.log('before mount');
+    },
+
+    mounted: function(){
+        console.log('mounted');
+        const ssTodos = sessionStorage.getItem('my-todo-list');
+        this.todoList = ssTodos ? JSON.parse(ssTodos) : [];
+    },
+
 })
